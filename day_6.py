3398e6277837033f5b9d63b09cda56a4eecae12d
Paul Way <pauleway@gmail.com>
2022-12-06 15:02:58 -0800

    First Push

--- day_6.py
+++ day_6.py
@@ -4,6 +4,11 @@ if __name__ == '__main__':
     data = load_as_string_array('data/day_6_data.txt')
     line = data[0]
     print(len(line))
+    for i in range(3, len(line)):
+        if(len(set(line[i-4:i])) == 4):
+            print(i)
+            break
+
     for i in range(13, len(line)):
         if(len(set(line[i-14:i])) == 14):
             print(i)
